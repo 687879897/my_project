a47198466e72a49501ffc31dd3ef69cba175507b
Marwan nassr <mrwann773@gmail.com>
2024-02-11 21:13:21 +0200

    add radio tab

--- lib/radio/radio_tab.dart
+++ lib/radio/radio_tab.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../utils/app_assets.dart';
+import '../utils/app_colors.dart';
+import '../utils/app_theme.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
@@ -8,9 +10,20 @@ class RadioTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(flex:3,child: Center(child: Image.asset("assets/assets/radio_image.png"))),
-          
+          Center(child: Image.asset("assets/assets/radio_image.png")),
+          Text("اذاعة القران الكريم",style:AppTheme.mediumTitleTextStyle ,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(onPressed: (){}, icon: Icon(Icons.skip_previous,size: 42,),color:AppColors.orange,),
+              IconButton(onPressed: (){}, icon: Icon(Icons.play_arrow,size: 52),color:AppColors.orange),
+              IconButton(onPressed: (){}, icon: Icon(Icons.skip_next,size: 42),color:AppColors.orange ,),
+
+            ],),
+
+
         ],
       ),
     );;
